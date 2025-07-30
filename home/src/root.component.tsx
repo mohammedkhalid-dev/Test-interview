a36677067e572043acb9ccd1540748473233325a
mohammedkhalid <mohammed.khalid@akra.ai>
2025-07-30 10:42:06 +0530

    latest changes

--- home/src/root.component.tsx
+++ home/src/root.component.tsx
@@ -7,8 +7,8 @@ import SingleCard from './single-card/single-card';
 export default function Root(props) {
   return <section className='app'>
     <SingleCard
-      title="Micro-Frontend"
-      description="Micro-frontend architectures decompose a front-end app into individual, semi-independent “microapps” working loosely together. This can help make large projects more manageable, e.g. when transitioning from legacy codebases."
+      title="Frontend"
+      description="frontend architectures decompose a front-end app into individual, semi-independent “microapps” working loosely together. This can help make large projects more manageable, e.g. when transitioning from legacy codebases."
     >
       <img className='micro-fe-img' src='https://miro.medium.com/max/1050/1*rxsVRHNFdG-6gvOIUGAdcw.jpeg'/>
 
